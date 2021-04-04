290c3a44da14c630920883bca0f3ee5503982154
Elnur Sardarov <sardarovelnur@gmail.com>
2021-04-04 12:58:34 +0300

    make main page header + connect postgresql

--- src/main/java/ru/itis/new_project/controllers/MainController.java
+++ src/main/java/ru/itis/new_project/controllers/MainController.java
@@ -3,7 +3,7 @@ package ru.itis.new_project.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
@@ -11,7 +11,7 @@ public class MainController {
     @GetMapping("/")
     public String greeting(Model model) {
         model.addAttribute("name", "stranger");
-        return "home";
+        return "index";
     }
 
 }
